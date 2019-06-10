3bc6b606f2a7803404f3ce3191ac50df80ef255f
vstarcan <2853239884@qq.com>
2019-06-11 00:18:08 +0800

    add

---
+++ testvsnet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testvsnet
+//
+//  Created by onesand on 2018/12/11.
+//  Copyright © 2018年 onesand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//#import "VSNet.h"
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
